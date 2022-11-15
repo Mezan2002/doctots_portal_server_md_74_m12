2459d068d5445242f41b301f06e1c6ed6dcf066f
Mezan2002 <mezanurr009@gmail.com>
2022-11-16 01:41:26 +0600

    Mongo DB connect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // require start
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // middlewears end
+
+// mongo DB setup start
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2ahck7i.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+console.log(uri);
+// mongo DB setup end
 
 // basic setup start
 
